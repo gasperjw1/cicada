732b953d235b45701adf97b1f3179c4020128654
Nigel Ferrer <nferrertech@gmail.com>
2021-05-28 19:59:14 -0400

    does it work?

--- server.go
+++ server.go
@@ -2,53 +2,76 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	// "github.com/gorilla/mux"
+
+	"github.com/gorilla/mux"
 	//"github.com/joho/godotenv"
 	// "storj.io/uplink"
 )
 
 func UploadFile(w http.ResponseWriter, r *http.Request) {
 
-	switch r.Method {
-	case "POST":
-		r.ParseMultipartForm(10 << 20)
+	// Maximum upload of 10 MB files
+	r.ParseMultipartForm(10 << 20)
 
-		// Get handler for filename, size and headers
-		file, handler, err := r.FormFile("file") //<- file from form
-		if err != nil {
-			fmt.Println("Error Retrieving the File")
-			fmt.Println(err)
-			return
-		}
-
-		defer file.Close()
-		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-		fmt.Printf("File Size: %+v\n", handler.Size)
-		fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-		// Create file
-		dst, err := os.Create(handler.Filename)
-		defer dst.Close()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Copy the uploaded file to the created file on the filesystem
-		if _, err := io.Copy(dst, file); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, "Successfully Uploaded File\n")
-
-	default:
-		fmt.Fprintf(w, "Sorry, only POST metherd is supported.")
+	// Get handler for filename, size and headers
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Printf("%s", reqBody)
+
+	// file, handler, err := r.FormFile("myFile")
+	// if err != nil {
+	// 	fmt.Println("Error Retrieving the File")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// defer file.Close()
+	// fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	// fmt.Printf("File Size: %+v\n", handler.Size)
+	// fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// switch r.Method {
+	// case "POST":
+	// 	r.ParseMultipartForm(10 << 20)
+
+	// 	// Get handler for filename, size and headers
+	// 	file, handler, err := r.FormFile("file") //<- file from form
+	// 	if err != nil {
+	// 		fmt.Println("Error Retrieving the File")
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
+
+	// 	defer file.Close()
+	// 	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	// 	fmt.Printf("File Size: %+v\n", handler.Size)
+	// 	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// 	// Create file
+	// 	dst, err := os.Create(handler.Filename)
+	// 	defer dst.Close()
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	// Copy the uploaded file to the created file on the filesystem
+	// 	if _, err := io.Copy(dst, file); err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+	// default:
+	// 	fmt.Fprintf(w, "Sorry, only POST metherd is supported.")
+	// }
+
 }
 
 // Main
@@ -73,12 +96,13 @@ func main() {
 	http.Handle("/", fileServer)
 
 	// go router
-	// router := mux.NewRouter()
-	//router.HandleFunc("/upload-file", UploadFile) //.Methods("POST")
+	router := mux.NewRouter()
+	router.HandleFunc("/upload-file", UploadFile).Methods("POST")
 	//router.Handle("/upload-file", UploadFile)
-	http.HandleFunc("/upload-file", UploadFile) //enctype="multipart/form-data"
+	// http.HandleFunc("/upload-file", UploadFile) //enctype="multipart/form-data"
 
 	fmt.Printf("Starting server at port 8080\n")
+	// fmt.Printf(poof)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
