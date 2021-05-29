38c7bae89ff727ec201a53c64111cf769f3f58e1
Yash Mahtani <ymahtani5911@bths.edu>
2021-05-29 13:06:43 -0400

    Using r.Body instead of r.FormFile to retrieve file in backend

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 	"text/template"
 	"time"
@@ -146,37 +145,39 @@ func UploadData(ctx context.Context, data []byte) error {
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 
 	// Maximum upload of 10 MB files
-	r.ParseMultipartForm(10 << 20)
+	// r.ParseMultipartForm(10 << 20)
 
-	// Get handler for filename, size and headers
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		return
-	}
+	// // Get handler for filename, size and headers
+	// file, handler, err := r.FormFile("myFile") //r.Body
+	// if err != nil {
+	// 	fmt.Println("Error Retrieving the File")
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	// defer file.Close()
+	// fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	// fmt.Printf("File Size: %+v\n", handler.Size)
+	// fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// Create file
-	dst, err := os.Create(handler.Filename)
-	defer dst.Close()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// // Create file
+	// dst, err := os.Create(handler.Filename)
+	// defer dst.Close()
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
-	// Copy the uploaded file to the created file on the filesystem
-	if _, err := io.Copy(dst, file); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// // Copy the uploaded file to the created file on the filesystem
+	// if _, err := io.Copy(dst, file); err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
 	// Converts *os.File into []byte
-	_, _ = dst.Seek(0, 0)
+	dst := r.Body
+	defer dst.Close()
+	// _, _ = dst.Seek(0, 0)
 	b, err := ioutil.ReadAll(dst)
 	if err != nil {
 		log.Fatal(err)
