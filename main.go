e223d4fd90e64116c3863d75f8b1ea50ed3534a2
Nigel Ferrer <nferrertech@gmail.com>
2021-05-28 20:53:53 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 	"net/http"
 	"os"
 	"text/template"
+	"time"
+	"github.com/joho/godotenv"
+	"storj.io/uplink"
+
+	"bytes"
+	"context"
+	"io/ioutil"
+	"log"
 )
+
+var accessT string
 
 // Compile templates on start of the application
 var templates = template.Must(template.ParseFiles("public/upload.html"))
@@ -16,7 +26,44 @@ func display(w http.ResponseWriter, page string, data interface{}) {
 	templates.ExecuteTemplate(w, page+".html", data)
 }
 
+// UploadAndDownloadData uploads the data to objectKey in
+// bucketName, using accessGrant.
+func UploadData(ctx context.Context, data []byte) error {
+	
+	bucketName = "bucket1"
+	objectKey = time.Now()
+
+	// Ensure the desired Bucket within the Project is created.
+	_, err = project.EnsureBucket(ctx, bucketName)
+	if err != nil {
+		return fmt.Errorf("could not ensure bucket: %v", err)
+	}
+
+	// Intitiate the upload of our Object to the specified bucket and key.
+	upload, err := project.UploadObject(ctx, bucketName, objectKey, nil)
+	if err != nil {
+		return fmt.Errorf("could not initiate upload: %v", err)
+	}
+
+	// Copy the data to the upload.
+	buf := bytes.NewBuffer(data)
+	_, err = io.Copy(upload, buf)
+	if err != nil {
+		_ = upload.Abort()
+		return fmt.Errorf("could not upload data: %v", err)
+	}
+
+	// Commit the uploaded object.
+	err = upload.Commit()
+	if err != nil {
+		return fmt.Errorf("could not commit uploaded object: %v", err)
+	}
+
+	return nil
+}
+
 func uploadFile(w http.ResponseWriter, r *http.Request) {
+
 	// Maximum upload of 10 MB files
 	r.ParseMultipartForm(10 << 20)
 
@@ -48,6 +95,7 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
+	UploadData(ctx, dst)
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,6 +108,30 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//Creates a project
+	var envs map[string]string
+	envs, err := godotenv.Read(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	accessGrant := envs["STORJACCESSGRANT"]
+
+	// Parse the Access Grant.
+	access, err := uplink.ParseAccess(accessGrant)
+	if err != nil {
+		return fmt.Errorf("could not parse access grant: %v", err)
+	}
+	
+	project, err := uplink.OpenProject(ctx, access)
+	if err != nil {
+		return fmt.Errorf("could not open project: %v", err)
+	}
+	defer project.Close()
+
+	accessT = access
+
 	// Upload route
 	http.HandleFunc("/upload", uploadHandler)
 
