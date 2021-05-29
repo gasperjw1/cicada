0ee93d642a884a0d4dd7e57a27ca0ba86a4bbba0
ViolentHajdini <lentihajdini@gmail.com>
2021-05-29 13:34:50 -0400

    upload update

--- UI-UX/src/Pages/Upload/Upload.js
+++ UI-UX/src/Pages/Upload/Upload.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { useSelector} from 'react-redux'
+import user from '../../Assets/user.png'
+import upload from '../../Assets/upload.png'
+import upload2 from '../../Assets/upload2.png'
+
 import './Upload.css'
 
 const Upload = () => {
+    const userWallet = useSelector(state=>state.walletReducer)
 
     const [selectedFile, uploadFile] = useState([])
     const [fileName, setFileName] = useState('Choose File')
@@ -13,6 +19,7 @@ const Upload = () => {
         console.log(selectedFile)
     }
 
+
     const submitUploads = async (event) => {
         event.preventDefault()
 
@@ -22,6 +29,7 @@ const Upload = () => {
             {
                 const formData = new FormData()
                 formData.append('fileName', selectedFile[i])
+                {console.log(selectedFile[i])}
                 const res = await axios.post('http://localhost:3001/upload', formData, {
                     headers: {
                         'Content-Type':'multipart/form-data'
@@ -49,7 +57,10 @@ const Upload = () => {
 
             <div className='Upload'>
                 <form onSubmit={submitUploads}>
-                    <input name='myFile' type='file' onChange={onFileChange}/>
+                    <input name='myFile' 
+                           type='file' 
+                           onChange={onFileChange}
+                           />
                     <input type='submit' value='Upload'/>
                 </form>
                 <button onClick={()=>{console.log(selectedFile)}}>selected file</button>
@@ -57,7 +68,6 @@ const Upload = () => {
             <div className="display-files-container">
                 {
                     selectedFile.map((fileHandle,index)=>{
-                    
                         return(
                             <div key={`${fileHandle}${index}`} className="display-files" >
                                 <p>{fileHandle.name}</p>
@@ -68,9 +78,38 @@ const Upload = () => {
                     })
                 }
             </div>
-                
-            
-        </div>
+            <div className='upload-container'>
+                <div className='upload-wrapper'>
+                    <div className="top-container">  
+                    <div className='user-info-container'>
+                        <img src={user} alt='user.png' className="user-wrapper"/>
+                        <div className="user-wallet-wrapper">0x916c5F7ddcce69F0…f1e2a4f</div>
+                        {console.log('Wallets:', userWallet)}
+                    </div>
+                    <div className="upload-file-wrapper">
+                        <form className="form-wrapper">
+                            <input 
+                            className="upload-input"
+                            name='myFile' 
+                            type='file' 
+                            id='file'
+                            onChange={onFileChange}
+                            />
+                            <label className="upload-file" for="file"> <img className="upload-img"src={upload2} alt="upload.png"/> Choose a file</label>
+                            <input className="upload-button"type='submit' value='Upload'></input>
+                        </form>
+
+                    </div>
+                    </div>
+                    <div className="break"/>
+                    <div className="static-text">
+                        <h1 className="static-h1">Name</h1> <h1 className="static-h1">Size</h1> <h1 className="static-h1">Upload Date</h1>
+                    </div>
+                    <div className="break"/>
+                    <button onClick={()=>{console.log(selectedFile)}}>selected file</button>
+                </div>
+                </div>
+            </div>
     )
 }
 
