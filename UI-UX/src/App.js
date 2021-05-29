7f6c6dccd6d3b18d8516288ac8657f6593aa9e53
ViolentHajdini <lentihajdini@gmail.com>
2021-05-29 18:21:04 -0400

    fixed minor issue with upload

--- UI-UX/src/App.js
+++ UI-UX/src/App.js
@@ -14,22 +14,6 @@ function App() {
     web3.eth.getAccounts((error, accounts) => {
       dispatch({type: WALLET_SUCCESS, payload:accounts})
     })
-    .then(accounts => {
-      web3
-        .listAccounts()
-        .then(wallet => {
-          if (wallet.length) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const REGISTERED_USER = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("REGISTERED_USER"))
-            const signer = provider.getSigner()
-            const address = ""
-            const abi = ""
-            const contract = new ethers.Contract(address, abi, signer)
-
-            contract.hasRole(REGISTERED_USER, wallet[0])
-          }
-        })
-    })
   }
 // hello {^V^}
   return (
--- UI-UX/src/Pages/Upload/Upload.js
+++ UI-UX/src/Pages/Upload/Upload.js
@@ -2,7 +2,8 @@ import React, { useState,useEffect } from 'react'
 import axios from 'axios'
 import { useSelector} from 'react-redux'
 import user from '../../Assets/user.png'
-import upload from '../../Assets/upload.png'
+import uploadIcon from '../../Assets/uploadicon.png'
+import myfilesIcon from '../../Assets/myfilesicon.png'
 import upload2 from '../../Assets/upload2.png'
 import Display from './displayUpload';
 
@@ -13,6 +14,7 @@ const Upload = () => {
 
     const [selectedFile, uploadFile] = useState([])
     const [fileName, setFileName] = useState('Choose File')
+    const [viewFiles, setViewFiles] = useState(false);
 
     const onFileChange = (event) => {
         uploadFile([...selectedFile, event.target.files[0]])
@@ -21,7 +23,7 @@ const Upload = () => {
     }
 
     useEffect(()=>{
-        const setNav = document.querySelector('.pricing');
+        const setNav = document.querySelector('.upload');
         setNav.childNodes[0].classList.add('active-nav-item');
     },[])
 
@@ -56,44 +58,95 @@ const Upload = () => {
         }));
 
     }
-    
+
+    const setView = () =>{
+        setViewFiles(true);
+    }
+    const setViewNT = () =>{
+        setViewFiles(false);
+    }
+    // if(!userWallet.wallets){
+    //     return(
+    //         <>
+    //             <div ClassName="no-account-landing">
+    //                 <p>Sorry, no account has been detected. If you don't have an account please register with PORTIS. If you are not subrcribed please consider doing so. Click here for more information PRICING</p>
+
+    //             </div>
+                    
+    //         </>
+    //     )
+    // }
     return (
         <div className='Container'>
             <div className='upload-container'>
-                <div className='upload-wrapper'>
-                    <div className="top-container">  
-                    <div className='user-info-container'>
-                        <img src={user} alt='user.png' className="user-wrapper"/>
-                        {
-                            userWallet.wallets
-                            ?<div className="user-wallet-wrapper">{userWallet.wallets}</div>
-                            : null
-                        }
-                    </div>
-                    <div className="upload-file-wrapper">
-                        <form className="form-wrapper">
-                            <input 
-                            className="upload-input"
-                            name='myFile' 
-                            type='file' 
-                            id='file'
-                            onChange={onFileChange}
-                            />
-                            <label className="upload-file" for="file"> <img className="upload-img"src={upload2} alt="upload.png"/> Choose a file</label>
-                            <input className="upload-button"type='submit' value='Upload'></input>
-                        </form>
-                    </div>
-                    </div>
-                    <div className="break"/>
-                    <div className="static-text">
-                        <h1 className="static-h1-one">Name</h1> <h1 className="static-h1-two">Size</h1> <h1 className="static-h1-three">Upload Date</h1>
-                    </div>
-                    <div className="break"/>
 
-                    <div className="second-container">
-                    <Display selectedFile={selectedFile} removeFile={removeFile} />
-                    </div>
+                <div className="sideNav">
+                    
+                        <div onClick={setViewNT}><img className='icon-upload' src={uploadIcon} alt='uploadicon.png'/> <span>Upload</span></div>
+                        <div onClick={setView} ><img className='icon-file' src={myfilesIcon} alt='myfilesicon.png'/><span>My Files</span></div>
+                 
                 </div>
+
+                {!viewFiles 
+                    ?<div className='upload-wrapper'>
+                        <div className="top-container">  
+                            <div className='user-info-container'>
+                                <img src={user} alt='user.png' className="user-wrapper"/>
+                                {
+                                    userWallet.wallets
+                                    ?<div className="user-wallet-wrapper">{userWallet.wallets}</div>
+                                    : null
+                                }
+                            </div>
+                            <div className="upload-file-wrapper">
+                                <form className="form-wrapper" onSubmit={submitUploads}>
+                                    <input 
+                                    className="upload-input"
+                                    name='myFile' 
+                                    type='file' 
+                                    id='file'
+                                    onChange={onFileChange}
+                                    />
+                                    <label className="upload-file" for="file"> <img className="upload-img"src={upload2} alt="upload.png"/> Choose a file</label>
+                                    <input className="upload-button"type='submit' value='Upload'></input>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="break"/>
+                        <div className="static-text">
+                            <h1 className="static-h1-one">Name</h1> <h1 className="static-h1-two">Size</h1> <h1 className="static-h1-three">Upload Date</h1>
+                        </div>
+                        <div className="break"/>
+
+                        <div className="second-container">
+                        <Display selectedFile={selectedFile} removeFile={removeFile} />
+                        </div>
+                    </div>
+                        
+                    :<div className="display-my-files">
+                        <div className="my-files-container">
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                            <div className="fluff"/>
+                        </div>
+                    </div>
+                }
                 </div>
             </div>
     )
