e10faeae6dafc884e9a05483c270015c5a5d9577
RandyMThio40 <37589527+RandyMThio40@users.noreply.github.com>
2021-05-29 15:33:56 -0400

    sex

--- UI-UX/src/Nav/Nav.js
+++ UI-UX/src/Nav/Nav.js
@@ -1,7 +1,13 @@
 import React,{useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import './Nav.css';
 
 export default function Nav(props){
+    const clear_underline = () =>{
+        const underline = document.querySelector('.active-nav-item');
+        underline?.classList.remove('active-nav-item');
+    }
+
     useEffect(()=>{
         const nav_items = document.querySelectorAll('.nav-item');
         nav_items.forEach((nav_item,index)=>{
@@ -21,17 +27,18 @@ export default function Nav(props){
     },[])
     return(
         <nav>
-            <div className="logo">
-                Cicada-Blockchain
+
+            <div className="logo" onClick={clear_underline}>
+                <Link to="/" target="_self">Cicada-Blockchain</Link>
             </div>
 
             <div className="nav-item">
                 <div className="underline"/>
-                Pricing
+                <Link to="/pricing" target="_self">Pricing</Link>
             </div>
             <div className="nav-item">
                 <div className="underline"/>
-                Upload
+                <Link to="/upload" target="_self">Upload</Link>
                
             </div>
             <button className="connect-portis-button" onClick={props.connect_wall} >
